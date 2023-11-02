93110fac04caab1d2dd7354733fbaaa49b5ca2f9
Michael Leighton <michael138leighton@gmail.com>
2023-11-02 16:06:09 -0500

    added js func

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+    const messageDiv = document.querySelector('.message');
+    const emailInput = document.querySelector('input[name="email"]');
+  
+    const displayMessage = (message, isError = false) => {
+      messageDiv.className = isError ? 'message error-message' : 'message';
+      messageDiv.innerHTML = message;
+    };
+  
+    const validateEmail = (email) => {
+      return email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) ? true : false;
+    };
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const email = emailInput.value.trim();
+  
+      if (!email || !validateEmail(email)) {
+        displayMessage('Please enter a valid email address.', true);
+        return;
+      }
+  
+      displayMessage(`Thank you! Your email address <strong>${email}</strong> has been added to our mailing list!`);
+    });
+  });
+  
